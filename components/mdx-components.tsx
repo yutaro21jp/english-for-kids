09a6ffa449e4e779c77f77559c99c770f76d6e5f
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2025-09-16 19:24:10 +0700

    「YouTube埋め込み」機能を追加しました。

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image"
 import { useMDXComponent } from "next-contentlayer/hooks"
 
+const YouTube = ({ id }: { id: string }) => (
+  <div className="aspect-w-16 aspect-h-9">
+    <iframe
+      src={`https://www.youtube.com/embed/${id}`}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      className="w-full h-full"
+    ></iframe>
+  </div>
+);
+
 const components = {
   Image,
+  YouTube,
 }
 
 interface MdxProps {
